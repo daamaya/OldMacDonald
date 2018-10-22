f02d0bd8eac2bb22cc5dd8ec1414f0131061ec66
daamaya <daamaya@s.sfusd.edu>
2018-10-22 14:08:42 -0700

    First commit

--- Cow.java
+++ Cow.java
@@ -1,4 +1,17 @@
-class Cow //implements Animal 
+class Cow implements Animal 
 {     
-     //your code here
+	private String myType;
+	private String mySound;
+	public Cow(String type, String sound)
+	{
+		myType = type;
+		mySound = sound;
+	}
+	public Cow()
+	{
+		myType = "Unkown";
+		mySound = "Unkown";
+	}
+	public getType(){return myType;}
+	public getSound(){return mySound;}
 }
